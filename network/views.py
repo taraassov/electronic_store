62c90a1de34886759d52284dd9410833dbe21891
taraassov <taraassov03@gmail.com>
2024-01-17 00:21:44 +0500

    views

--- network/views.py
+++ network/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from network.models import Network
+from network.serializers import NetworkSerializer
 
-# Create your views here.
+
+class NetworksCreateAPIView(generics.CreateAPIView):
+    serializer_class = NetworkSerializer
+
+
+class NetworksListAPIView(generics.ListAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+
+class NetworksRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+
+class NetworksUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+
+class NetworksDestroyAPIView(generics.DestroyAPIView):
+    queryset = Network.objects.all()
